b39b2a9ee96d78ed619ea57b0dd43d429f82c5d3
David Conner <agilebluefox@gmail.com>
2016-05-24 11:07:31 -0400

    Added the .ryu-cool html class and css styles, added jQuery functions for the keydown and keyup events

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     // and replace it with the ready gif
     $('.ryu').mouseenter(function() {
         $('.ryu-still').hide();
+        $('.ryu-cool').hide();
         $('.ryu-ready').show();
     })
     // When the cursor leaves the div, repalce the ready gif
@@ -16,11 +17,18 @@ $(document).ready(function() {
     .mousedown(function() {
         console.log('mousedown');
         // Play the audio clip
+        playHadouken();
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
-        // Add the ball of fire
-        $('.hadouken').show();
-        // Animate the ball of fire across the screen
+        // Add the ball of fire and animate it across the screen
+        $('.hadouken').finish().show().animate(
+            { 'left' : '1020px' },
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '535px');
+            }
+        );
     })
     // When the mouse button is released, switch back to the ready image
     .mouseup(function() {
@@ -28,4 +36,34 @@ $(document).ready(function() {
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
     });
+    // When the 'x' key is pressed, make ryu cool
+    // Select the body tag (or the document?)
+    // Using the example from the API docs ....
+    $( "body" ).keydown(function(x) {
+        // Check for a specific key - 'x'
+        if(x.which == 88) {
+            // debugging message
+            console.log( "Handler for .keydown() called." );
+            // Hide the other images and show the "cool" image
+            $('.ryu-ready').hide();
+            $('.ryu-still').hide();
+            $('.ryu-cool').show();
+        }
+    })
+    // Switch out the cool image when the 'x' key is released
+    .keyup(function(x) {
+        if(x.which == 88) {
+            // debugging message
+            console.log( "Handler for .keyup() called." );
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+        }
+    });
 });
+
+// Function to play the sound when ryu fires a shot
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
